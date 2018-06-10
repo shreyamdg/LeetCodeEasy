14817e93698bcb22cbf4cba8299f6acad0128c2c
shreyam15 <shreyam.duttagupta@gmail.com>
2018-06-09 23:37:40 -0500

    romanToInt

---
+++ src/RomantoInteger.java
@@ -0,0 +1,53 @@
+
+public class RomantoInteger {
+	
+	public static int romanToInt(String s) {
+        int num = 0;
+        char [] charArr = s.toUpperCase().toCharArray();
+        for(int i=0; i<charArr.length; i++) {
+        	
+        	if(charArr[i] == 'I' && (i+1) < charArr.length && charArr[i+1] == 'V') {
+        		num = num + 4;
+        		i++;
+        	}else if(charArr[i] == 'I' && (i+1) < charArr.length && charArr[i+1] == 'X') {
+        		num = num + 9;
+        		i++;
+        	}else if(charArr[i] == 'I') {
+        		num = num + 1;
+        	}else if(charArr[i] == 'V') {
+        		num = num + 5;
+        	}else if(charArr[i] == 'X' && (i+1) < charArr.length && charArr[i+1] == 'L') {
+        		num = num + 40;
+        		i++;
+        	}else if(charArr[i] == 'X' && (i+1) < charArr.length && charArr[i+1] == 'C') {
+        		num = num + 90;
+        		i++;
+        	}else if(charArr[i] == 'X') {
+        		num = num + 10;
+        	}else if(charArr[i] == 'L') {
+        		num = num + 50;
+        	}else if(charArr[i] == 'C' && (i+1) < charArr.length && charArr[i+1] == 'D') {
+        		num = num + 400;
+        		i++;
+        	}else if(charArr[i] == 'C' && (i+1) < charArr.length && charArr[i+1] == 'M') {
+        		num = num + 900;
+        		i++;
+        	}else if(charArr[i] == 'C') {
+        		num = num + 100;
+        	}else if(charArr[i] == 'D') {
+        		num = num + 500;
+        	}else if(charArr[i] == 'M') {
+        		num = num + 1000;
+        	}
+        }
+       
+        return num;
+	}
+	
+	
+	
+	public static void main(String[] args) {
+		System.out.println(romanToInt("MCMXCIV"));
+	}
+
+}
