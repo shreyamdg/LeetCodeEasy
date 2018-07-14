a5768b9fb5ca1f2d5d3bccb3fcb848026752846c
shreyam15 <shreyam.duttagupta@gmail.com>
2018-07-13 20:05:43 -0500

    to check if a number is palindrome

---
+++ src/PalindromeNumber.java
@@ -0,0 +1,23 @@
+
+public class PalindromeNumber {
+
+	public static boolean isPalindrome(int x) {
+		int rev = 0;
+		if (x < 0)
+			return false;
+		int num = x;
+		while (x != 0) {
+			rev = rev * 10 + (x % 10);
+			x = x / 10;
+		}
+
+		if (num == rev)
+			return true;
+		else
+			return false;
+	}
+
+	public static void main(String[] args) {
+		System.out.println(isPalindrome(121));
+	}
+}
