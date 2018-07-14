5e7b273e23b2d9634756b7fff3948a7b942e70fe
shreyam15 <shreyam.duttagupta@gmail.com>
2018-07-13 20:09:50 -0500

    code for rotating array from both sides

---
+++ src/RotateArray.java
@@ -0,0 +1,45 @@
+
+public class RotateArray {
+	/*
+	 * For Left rotation
+	 *  A = Reverse(0 to k - 1)
+	 *  B = Reverse(k to arraylength -1)
+	 *  Then Reverse whole Array ( 0 to Arraylength-1)
+	 */
+	
+	/*
+	 * For Right rotation
+	 *  A = Reverse(0 to arraylength - k - 1)
+	 *  B = Reverse(arraylength-k to arraylength -1)
+	 *  Then Reverse whole Array ( 0 to Arraylength-1)
+	 */
+    
+    public static void rotate(int[] nums, int k) {
+        
+       int arrlength = nums.length;        
+        if(k> arrlength){
+            k = k%arrlength;
+        }
+       reverseArray(nums, 0, arrlength-k-1);
+       reverseArray(nums, arrlength-k, arrlength - 1);
+       reverseArray(nums, 0, arrlength - 1);
+    }
+    
+     static void reverseArray(int arr[], int start,
+                             int end)
+    {
+        while (start < end)
+        {
+           int temp = arr[start];
+           arr[start] = arr[end];
+           arr[end] = temp;
+           start++;
+           end--;
+         }
+    }
+     
+     public static void main(String[] args) {
+    	int [] arr = new int [] {1,2,3,4,5,6,7};
+ 		rotate(arr, 3);
+ 	}
+}
