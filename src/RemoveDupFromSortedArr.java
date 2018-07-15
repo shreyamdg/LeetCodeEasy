54fc6830b8bddba1a8c09d32127f5f05af89a0c4
shreyam15 <shreyam.duttagupta@gmail.com>
2018-07-15 00:44:30 -0500

    with another solution

--- src/RemoveDupFromSortedArr.java
+++ src/RemoveDupFromSortedArr.java
@@ -18,14 +18,33 @@ public class RemoveDupFromSortedArr {
 			}
 
 		}
-
 		return last;
 	}
 
+	
+	// Another solution
+	/*public static int removeDuplicates(int[] nums) {
+		int i = 1;
+		int j = 1;
+
+		while (j < nums.length) {
+			if (nums[j] != nums[j - 1]) {
+				move(nums, i, j);
+				++i;
+			}
+			++j;
+		}
+		return i;
+	}
+
+	public static void move(int[] nums, int i, int j) {
+		nums[i] = nums[j];
+	}*/
+
 	public static void main(String[] args) {
-		int[] arr = new int[] { 0, 0, 1, 1, 1, 2, 2, 3, 3, 4};
+		int[] arr = new int[] { 0, 0, 1, 1, 1, 2, 2, 3, 3, 4 };
 		removeDuplicates(arr);
-		for(int i : arr) 
+		for (int i : arr)
 			System.out.println(i);
 	}
 }
