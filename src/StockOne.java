7afa5bf5dca614b077e527f3bbe49b40f5d4a440
shreyam15 <shreyam.duttagupta@gmail.com>
2018-07-14 22:40:35 -0500

    formatted codes

--- src/StockOne.java
+++ src/StockOne.java
@@ -1,30 +1,30 @@
 
 public class StockOne {
 	public static int maxProfit(int[] prices) {
-        int size =  prices.length;
-        if(size == 0){
-            return 0;
-        }
-        
-        int diff = 0;
-        int min = prices[0];
-        
-        for(int i=1; i<size; i++){
-            if(prices[i] - min > diff){
-                diff = prices[i] - min;
-            }
-            
-            if(prices[i] < min ){
-                min = prices[i];
-            }
-            
-        }
-        
-        return diff;
-    }
-	
+		int size = prices.length;
+		if (size == 0) {
+			return 0;
+		}
+
+		int diff = 0;
+		int min = prices[0];
+
+		for (int i = 1; i < size; i++) {
+			if (prices[i] - min > diff) {
+				diff = prices[i] - min;
+			}
+
+			if (prices[i] < min) {
+				min = prices[i];
+			}
+
+		}
+
+		return diff;
+	}
+
 	public static void main(String[] args) {
-    	int [] arr = new int [] {7,1,5,3,6,4};
-    	maxProfit(arr);
- 	}
+		int[] arr = new int[] { 7, 1, 5, 3, 6, 4 };
+		maxProfit(arr);
+	}
 }
