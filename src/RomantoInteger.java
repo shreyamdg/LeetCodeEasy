27d793454b2a06b7ef59c63985485cd9e5ff71bf
shreyam15 <shreyam.duttagupta@gmail.com>
2018-07-14 23:22:48 -0500

    added comments

--- src/RomantoInteger.java
+++ src/RomantoInteger.java
@@ -1,6 +1,17 @@
 
 public class RomantoInteger {
 	
+	/*
+	  Symbol       Value
+		I             1
+		V             5
+		X             10
+		L             50
+		C             100
+		D             500
+		M             1000
+	 */
+	
 	public static int romanToInt(String s) {
         int num = 0;
         char [] charArr = s.toUpperCase().toCharArray();
