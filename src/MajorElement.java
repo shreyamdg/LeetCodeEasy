0c130a905cf3d3175cc7ca5c93e9314e99d989ea
shreyam15 <shreyam.duttagupta@gmail.com>
2018-07-15 15:20:59 -0500

    with another solution

--- src/MajorElement.java
+++ src/MajorElement.java
@@ -1,3 +1,4 @@
+import java.util.Arrays;
 
 public class MajorElement {
 	public static int majorityElement(int[] nums) {
@@ -11,6 +12,29 @@ public class MajorElement {
 				--count;
 		}
 		return major;
+	}
+
+	public int majorityElementAnotherSolution(int[] nums) {
+		Arrays.sort(nums);
+		int key = nums[0];
+		int key2 = 0;
+		int count = 1;
+		int count2 = 0;
+		for (int i = 1; i < nums.length; i++) {
+			if (nums[i] == nums[i - 1]) {
+				count++;
+				key = nums[i];
+			} else if (count > count2) {
+				count2 = count;
+				count = 1;
+				key2 = nums[i - 1];
+			}
+		}
+
+		if (count > count2)
+			return key;
+		else
+			return key2;
 	}
 
 	public static void main(String[] args) {
