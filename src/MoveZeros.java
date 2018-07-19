c2c1b893f757b0ee3d86c8c227d330b6b6b6fc1e
shreyam15 <shreyam.duttagupta@gmail.com>
2018-07-18 23:24:47 -0500

    moving zeros inital commit

---
+++ src/MoveZeros.java
@@ -0,0 +1,15 @@
+
+public class MoveZeros {
+	
+	 public static void moveZeroes(int[] nums) {
+	        
+	  }
+
+	
+	public static void main(String[] args) {
+		int[] arr = new int[] {0,1,0,3,12};
+		moveZeroes(arr);
+		for (int i : arr)
+			System.out.println(i);
+	}
+}
