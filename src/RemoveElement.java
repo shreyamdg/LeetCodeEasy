f5fc021af9db0d0bfda899995312600151b937de
shreyam15 <shreyam.duttagupta@gmail.com>
2018-07-18 23:58:46 -0500

    removing element val

---
+++ src/RemoveElement.java
@@ -0,0 +1,13 @@
+
+public class RemoveElement {
+	 public static int removeElement(int[] nums, int val) {
+		 return 0;
+	 }
+
+	public static void main(String[] args) {
+		int[] arr = new int[] { 0, 1, 0, 3, 12 };
+		System.out.println(removeElement(arr, 0));
+		
+			
+	}
+}
