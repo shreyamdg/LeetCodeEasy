65e563a2304b72a7c4dc85e1666e3fc1e82320ce
shreyam15 <shreyam.duttagupta@gmail.com>
2018-07-18 23:29:38 -0500

    code changes for moving zeros

--- src/MoveZeros.java
+++ src/MoveZeros.java
@@ -1,13 +1,23 @@
 
 public class MoveZeros {
-	
-	 public static void moveZeroes(int[] nums) {
-	        
-	  }
 
-	
+	public static void moveZeroes(int[] nums) {
+		int j = 0;
+		for (int i = 0; i < nums.length; i++) {
+			if (nums[i] != 0) {
+				nums[j] = nums[i];
+				j++;
+			}
+
+		}
+
+		for (int k = j; k < nums.length; k++) {
+			nums[k] = 0;
+		}
+	}
+
 	public static void main(String[] args) {
-		int[] arr = new int[] {0,1,0,3,12};
+		int[] arr = new int[] { 0, 1, 0, 3, 12 };
 		moveZeroes(arr);
 		for (int i : arr)
 			System.out.println(i);
