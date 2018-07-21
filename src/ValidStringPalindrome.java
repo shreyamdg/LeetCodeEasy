37970483c80e39ee2a8756db77806ea01d6ddf9d
shreyam15 <shreyam.duttagupta@gmail.com>
2018-07-20 23:47:18 -0500

    checking a valid string palindrome

---
+++ src/ValidStringPalindrome.java
@@ -0,0 +1,32 @@
+
+public class ValidStringPalindrome {
+
+	public static boolean isPalindrome(String s) {
+		if (s.isEmpty())
+			return true;
+
+		String str = s.toLowerCase().replaceAll("[^a-zA-Z0-9]", "");
+		char[] arr = str.toCharArray();
+		int last = str.length() - 1;
+		int first = 0;
+
+		while (first < last) {
+			char ch = arr[first];
+			arr[first] = arr[last];
+			arr[last] = ch;
+			first++;
+			last--;
+		}
+
+		String newStr = new String(arr);
+
+		if (newStr.equals(str))
+			return true;
+
+		return false;
+	}
+
+	public static void main(String[] args) {
+		System.out.println(isPalindrome("A man, a plan, a canal: Panama"));
+	}
+}
