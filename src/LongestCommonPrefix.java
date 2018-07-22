522cca6fd51c3daff2143e2aba9ad0290d69ca52
shreyam15 <shreyam.duttagupta@gmail.com>
2018-07-21 23:31:41 -0500

    longest common prefix in a array

---
+++ src/LongestCommonPrefix.java
@@ -0,0 +1,34 @@
+import java.util.Arrays;
+
+public class LongestCommonPrefix {
+
+	public static String longestCommonPrefix(String[] strs) {
+		StringBuilder result = new StringBuilder();
+
+		if (strs != null && strs.length > 0) {
+
+			Arrays.sort(strs);
+		}
+		
+		for(String s: strs)
+			System.out.println(s);
+
+		char[] a = strs[0].toCharArray();
+		char[] b = strs[strs.length - 1].toCharArray();
+
+		for (int i = 0; i < a.length; i++) {
+			if (b.length > i && b[i] == a[i]) {
+				result.append(b[i]);
+			} else {
+				return result.toString();
+			}
+		}
+		return result.toString();
+	}
+
+	public static void main(String[] args) {
+		String[] strs = new String[] { "flower", "floaw", "floght" };
+		System.out.println(longestCommonPrefix(strs));
+	}
+
+}
