86d211095513a8680f630cf8b76710776ea1fe86
shreyam15 <shreyam.duttagupta@gmail.com>
2018-07-22 22:32:31 -0500

    search and insert codes

---
+++ src/SearchInsertPosition.java
@@ -0,0 +1,22 @@
+
+public class SearchInsertPosition {
+
+	public static int searchInsert(int[] nums, int target) {
+		int index = 0;
+		for (int i = 0; i < nums.length; i++) {
+			if (nums[i] == target) {
+				return i;
+			}
+			if (index == 0 && nums[i] > target) {
+				index = i;
+			}
+		}
+		return index;
+	}
+
+	public static void main(String[] args) {
+		int[] arr = new int[] {1,3,5,6};
+		System.out.println(searchInsert(arr, 7));
+	}
+
+}
