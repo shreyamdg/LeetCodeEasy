184559bc8b0c8821fc0d0f13105a4f5a6770d285
shreyam15 <shreyam.duttagupta@gmail.com>
2018-07-22 22:48:35 -0500

    another solution

--- src/LengthofLastWord.java
+++ src/LengthofLastWord.java
@@ -1,12 +1,23 @@
 
 public class LengthofLastWord {
 	public static int lengthOfLastWord(String s) {
-        String []strArr = s.split("\\s");
-        if(strArr.length == 0)
-            return 0;
-        else
-         return strArr[strArr.length-1].length();
-    }
+		String[] strArr = s.split("\\s");
+		if (strArr.length == 0)
+			return 0;
+		else
+			return strArr[strArr.length - 1].length();
+	}
+	
+	//Time Efficient.
+	public static int anotherSolution(String s) {
+		s = s.trim();
+		String lastWord = s.substring(s.lastIndexOf(" ") + 1);
+		if (lastWord.isEmpty() || lastWord == null) {
+			return 0;
+		}
+		return lastWord.length();
+	}
+
 	public static void main(String[] args) {
 		System.out.println(lengthOfLastWord("     "));
 
