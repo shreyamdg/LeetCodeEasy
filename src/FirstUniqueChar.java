6a25941ddfb0da12add5bee2a4fa1ae68ee1e5cc
shreyam15 <shreyam.duttagupta@gmail.com>
2018-07-22 19:33:22 -0500

    another solution

--- src/FirstUniqueChar.java
+++ src/FirstUniqueChar.java
@@ -1,7 +1,7 @@
 import java.util.HashMap;
 
 public class FirstUniqueChar {
-	public static int firstUniqChar(String s) {
+	/*public static int firstUniqChar(String s) {
         HashMap<Character, Integer> map = new HashMap<>();
         for(char ch : s.toCharArray()){
             if(!map.containsKey(ch)){
@@ -18,9 +18,29 @@ public class FirstUniqueChar {
             
             return -1;
         
-    }
+    }*/
+	
+	// Without extra space. 
+	
+	public static int firstUniqChar(String s) {
+		int [] chars = new int[128];
+		for(int i = 0; i<s.length(); i++) {
+		    int val = s.charAt(i);
+            chars[val]=chars[val] + 1;
+		}
+        
+        char [] arr = s.toCharArray();
+        
+        for(int j = 0; j<s.length(); j++){
+            int val = arr[j];
+            if(chars[val] == 1){
+                return j;
+            }
+        }
+		return -1;
+		}
 	public static void main(String[] args) {
-		System.out.println(firstUniqChar("leetcode"));
+		System.out.println(firstUniqChar("loveleetcode"));
 	}
 
 }
