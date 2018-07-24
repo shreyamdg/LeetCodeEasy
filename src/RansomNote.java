3a2305f9e007c4f35b5bd5c7f2d9447caa513111
shreyam15 <shreyam.duttagupta@gmail.com>
2018-07-23 22:02:17 -0500

    ransom note problem. Asked in Kroger

---
+++ src/RansomNote.java
@@ -0,0 +1,25 @@
+
+public class RansomNote {
+	public static boolean canConstruct(String ransomNote, String magazine) {
+		int[] arr = new int[128];
+
+		for (int i = 0; i < magazine.length(); i++) {
+			int val = magazine.charAt(i);
+			arr[val] = arr[val] + 1;
+		}
+
+		for (int i = 0; i < ransomNote.length(); i++) {
+			int val = ransomNote.charAt(i);
+			arr[val] = arr[val] - 1;
+			if (arr[val] < 0)
+				return false;
+		}
+		return true;
+	}
+
+	public static void main(String[] args) {
+		System.out.println(canConstruct("aa", "aab"));
+
+	}
+
+}
