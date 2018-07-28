dd11df8f10240295bb3e9fd75e6b8928b3e72c62
shreyam15 <shreyam.duttagupta@gmail.com>
2018-07-28 11:53:24 -0500

    added support for uppercase chars

--- src/ReverseVowels.java
+++ src/ReverseVowels.java
@@ -6,14 +6,18 @@ public class ReverseVowels {
         
         StringBuilder stringVowel = new StringBuilder();
         for(int i = 0; i < sb.length(); i++) {
-        	if(sb.charAt(i) == 'a' || sb.charAt(i) == 'e' || sb.charAt(i) == 'i'|| sb.charAt(i) == 'o'|| sb.charAt(i) == 'u') {
+        	if(sb.charAt(i) == 'a' || sb.charAt(i) == 'e' || sb.charAt(i) == 'i'|| sb.charAt(i) == 'o'|| sb.charAt(i) == 'u'
+                 || sb.charAt(i) == 'A' || sb.charAt(i) == 'E' || sb.charAt(i) == 'I' || sb.charAt(i) == 'O' || 
+                    sb.charAt(i) == 'U') {
         		stringVowel.append(sb.charAt(i));
         	}
         }
         stringVowel.reverse();
         int count = 0;
         for(int i = 0; i<sb.length(); i++) {
-        	if(sb.charAt(i) == 'a' || sb.charAt(i) == 'e' || sb.charAt(i) == 'i'|| sb.charAt(i) == 'o'|| sb.charAt(i) == 'u') {
+        	if(sb.charAt(i) == 'a' || sb.charAt(i) == 'e' || sb.charAt(i) == 'i'|| sb.charAt(i) == 'o'|| sb.charAt(i) == 'u'
+                 || sb.charAt(i) == 'A' || sb.charAt(i) == 'E' || sb.charAt(i) == 'I' || sb.charAt(i) == 'O' || 
+                    sb.charAt(i) == 'U') {
         		sb.setCharAt(i, stringVowel.charAt(count));
         		count++;
         	}
@@ -21,7 +25,7 @@ public class ReverseVowels {
         
         return sb.toString();
     }
-    
+
 	public static void main(String[] args) {
 		System.out.println(reverseVowels("leetcode"));
 
