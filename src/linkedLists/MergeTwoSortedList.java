c35d1b6950783fa4c1a5fd817fb3975c14f20fae
shreyam15 <shreyam.duttagupta@gmail.com>
2018-07-29 15:00:03 -0500

    code for merging two lists

---
+++ src/linkedLists/MergeTwoSortedList.java
@@ -0,0 +1,41 @@
+package linkedLists;
+
+class ListNode {
+	int val;
+	ListNode next;
+
+	ListNode(int x) {
+		val = x;
+	}
+}
+
+public class MergeTwoSortedList {
+
+	public static ListNode mergeTwoLists(ListNode l1, ListNode l2) {
+
+		if (l1 == null)
+			return l2;
+		if (l2 == null)
+			return l1;
+
+		ListNode head = new ListNode(0);
+		ListNode curr = head;
+		while (l1 != null && l2 != null) {
+			if (l1.val <= l2.val) {
+				curr.next = l1;
+				l1 = l1.next;
+			} else {
+				curr.next = l2;
+				l2 = l2.next;
+			}
+			curr = curr.next;
+		}
+
+		if (l1 != null)
+			curr.next = l1;
+		else if (l2 != null)
+			curr.next = l2;
+
+		return head.next;
+	}
+}
