45023b526f823467c5574f08fe6e62c1d9988141
shreyam15 <shreyam.duttagupta@gmail.com>
2018-07-29 19:47:16 -0500

    removed ListNode extract

--- src/linkedLists/MergeTwoSortedList.java
+++ src/linkedLists/MergeTwoSortedList.java
@@ -1,14 +1,5 @@
 package linkedLists;
 
-class ListNode {
-	int val;
-	ListNode next;
-
-	ListNode(int x) {
-		val = x;
-	}
-}
-
 public class MergeTwoSortedList {
 
 	public static ListNode mergeTwoLists(ListNode l1, ListNode l2) {
