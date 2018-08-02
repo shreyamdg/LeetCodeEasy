cb6b1192139639983c9e9d38048a0b24c12efaf2
shreyam15 <shreyam.duttagupta@gmail.com>
2018-08-01 22:22:49 -0500

    another solution. 1 ms less runtime

--- src/linkedLists/RemoveElements.java
+++ src/linkedLists/RemoveElements.java
@@ -14,7 +14,6 @@ public class RemoveElements {
 				continue;
 			}
 			ListNode curr = pre.next;
-
 			if (curr != null && curr.val == val) {
 				pre.next = curr.next;
 			} else {
@@ -23,4 +22,20 @@ public class RemoveElements {
 		}
 		return head;
 	}
+	
+	public ListNode removeElementsAnotherSolution(ListNode head, int val) {
+        ListNode dummy = new ListNode(0);
+        dummy.next = head;
+        head = dummy;
+        
+        while (head.next!=null) {
+            if (head.next.val==val) {
+                 head.next=head.next.next;
+                
+            }
+            else
+            head=head.next;
+        }
+        return dummy.next;
+    }
 }
