bc403f0a42dfbb7a7881674edf2024d87d4140d5
shreyam15 <shreyam.duttagupta@gmail.com>
2018-08-04 01:15:11 -0500

    tree traversal code

--- src/trees/BSTImplementation.java
+++ src/trees/BSTImplementation.java
@@ -43,5 +43,41 @@ public class BSTImplementation {
 			}
 			return false;
 		}
+		
+		public void printInOrder() {
+			if(left != null) {
+				left.printInOrder();
+			}
+			
+			System.out.println(data);
+			
+			if(right != null) {
+				right.printInOrder();
+			}
+		}
+		
+		public void printPreOrder() {
+			System.out.println(data);
+			
+			if(left != null) {
+				left.printInOrder();
+			}
+			
+			if(right != null) {
+				right.printInOrder();
+			}
+		}
+		
+		public void printPostOrder() {
+			if(left != null) {
+				left.printInOrder();
+			}
+			
+			if(right != null) {
+				right.printInOrder();
+			}
+			
+			System.out.println(data);
+		}
 	}
 }
