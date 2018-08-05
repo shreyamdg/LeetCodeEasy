efcc022b853d159c3be0a05d249256c66d41bf2d
shreyam15 <shreyam.duttagupta@gmail.com>
2018-08-04 23:22:57 -0500

    added delete method

--- src/trees/BSTImplementation.java
+++ src/trees/BSTImplementation.java
@@ -82,5 +82,36 @@ public class BSTImplementation {
 			System.out.println(root.data);
 		}
 
+		public Node deleteNode(Node root, int val) {
+			if (root == null) {
+				return root;
+			}
+
+			if (val < root.data) {
+				root.left = deleteNode(root.left, val);
+			} else if (val > root.data) {
+				root.right = deleteNode(root.right, val);
+			} else {
+				if (root.left == null)
+					return root.right;
+				else if (root.right == null)
+					return root.left;
+
+				root.data = minValue(root.right);
+
+				root.right = deleteNode(root.right, root.data);
+			}
+
+			return root;
+		}
+
+		int minValue(Node root) {
+			int minv = root.data;
+			while (root.left != null) {
+				minv = root.left.data;
+				root = root.left;
+			}
+			return minv;
+		}
 	}
 }
