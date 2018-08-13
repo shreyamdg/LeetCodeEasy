775f769067a6c8f8500507199f0daee9c4a3d18f
shreyam15 <shreyam.duttagupta@gmail.com>
2018-08-12 19:40:19 -0500

    added optimized solution

--- src/arraysAndstrings/ValidAnagram.java
+++ src/arraysAndstrings/ValidAnagram.java
@@ -32,4 +32,28 @@ public class ValidAnagram {
 		}
 		return true;
 	}
+
+	public boolean isAnagramTwo(String s, String t) {
+
+		if (s.isEmpty() && t.isEmpty())
+			return true;
+		if (s.length() != t.length())
+			return false;
+
+		int[] arr = new int[26];
+
+		for (char c : s.toCharArray()) {
+			arr[c - 'a'] = arr[c - 'a'] + 1;
+		}
+
+		for (char c : t.toCharArray()) {
+			arr[c - 'a'] = arr[c - 'a'] - 1;
+		}
+
+		for (int i = 0; i < 26; i++) {
+			if (arr[i] != 0)
+				return false;
+		}
+		return true;
+	}
 }
