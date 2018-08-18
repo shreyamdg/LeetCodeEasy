0087dc54eeb597267e1c6f57bce9737f7c873260
shreyam15 <shreyam.duttagupta@gmail.com>
2018-08-17 21:12:26 -0500

    one edit distance without DP

---
+++ src/arraysAndstrings/OneEditDistance.java
@@ -0,0 +1,39 @@
+package arraysAndstrings;
+
+public class OneEditDistance {
+
+	public boolean solution(String str1, String str2) {
+		int length1 = str1.length();
+		int length2 = str2.length();
+
+		if (Math.abs(length1 - length2) > 1) {
+			return false;
+		}
+
+		int edits = 0;
+
+		int i = 0, j = 0;
+		while (i < length1 && j < length2) {
+			if (str1.charAt(i) != str2.charAt(j)) {
+				if (edits == 1)
+					return false;
+				if (length1 > length2) {
+					i++;
+				} else if (length2 > length1) {
+					j++;
+				} else {
+					i++;
+					j++;
+				}
+
+				edits++;
+			} else {
+				i++;
+				j++;
+			}
+		}
+
+		return true;
+	}
+
+}
