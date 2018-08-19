fa4c5af926f9f87267045f0183ad44521249e320
shreyam15 <shreyam.duttagupta@gmail.com>
2018-08-19 16:17:05 -0500

    distribution of candies

---
+++ src/arraysAndstrings/DistributeCandies.java
@@ -0,0 +1,17 @@
+package arraysAndstrings;
+
+import java.util.Arrays;
+
+public class DistributeCandies {
+	public int distributeCandies(int[] candies) {
+		Arrays.sort(candies);
+		int count = 1;
+		for (int i = 1; i < candies.length && count < (candies.length / 2); i++) {
+			if (candies[i] > candies[i - 1]) {
+				count++;
+			}
+		}
+
+		return count;
+	}
+}
