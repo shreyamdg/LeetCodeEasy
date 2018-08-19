cd07efde8e3702e547a2c3ea74ab3f6795011cc9
shreyam15 <shreyam.duttagupta@gmail.com>
2018-08-18 21:06:00 -0500

    if the input is in string and not inplace

--- src/arraysAndstrings/CompressedString.java
+++ src/arraysAndstrings/CompressedString.java
@@ -17,4 +17,27 @@ public class CompressedString {
 		}
 		return indexAns;
 	}
+
+	public static String compressString(String s) {
+		StringBuilder sb = new StringBuilder();
+		int count = 0;
+
+		for (int i = 0; i < s.length(); i++) {
+			count++;
+
+			if (i + 1 < s.length() && s.charAt(i) == s.charAt(i + 1)) {
+				continue;
+			} else {
+				sb.append(s.charAt(i));
+				sb.append(String.valueOf(count));
+				count = 0;
+			}
+		}
+
+		return sb.toString();
+	}
+
+	public static void main(String[] args) {
+		System.out.println(compressString("aabcccccaaa"));
+	}
 }
