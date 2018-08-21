098f049eaff2e1e03f7202ad835472ca62f9274b
shreyam15 <shreyam.duttagupta@gmail.com>
2018-08-20 21:53:18 -0500

    o(n) approach

--- src/arraysAndstrings/StudentAttendenceRecord.java
+++ src/arraysAndstrings/StudentAttendenceRecord.java
@@ -1,5 +1,25 @@
 package arraysAndstrings;
 
 public class StudentAttendenceRecord {
+	public boolean checkRecord(String s) {
+		int countA = 0;
+		int countL = 0;
+		for (int i = 0; i < s.length(); i++) {
+			if (s.charAt(i) == 'A') {
+				countA++;
+				countL = 0;
+			} else if (s.charAt(i) == 'L') {
+				countL++;
+			} else {
+				countL = 0;
+			}
 
+			if (countA > 1) {
+				return false;
+			} else if (countL > 2) {
+				return false;
+			}
+		}
+		return true;
+	}
 }
