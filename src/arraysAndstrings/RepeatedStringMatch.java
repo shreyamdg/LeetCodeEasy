5a51cf8c97d6d28e726d61af3b91547299989109
shreyam15 <shreyam.duttagupta@gmail.com>
2018-08-29 18:54:00 -0500

    repeated string match using stringbuilder

---
+++ src/arraysAndstrings/RepeatedStringMatch.java
@@ -0,0 +1,17 @@
+package arraysAndstrings;
+
+public class RepeatedStringMatch {
+	public int repeatedStringMatch(String A, String B) {
+		int count = 0;
+		StringBuilder sb = new StringBuilder();
+		while (sb.length() < B.length()) {
+			sb.append(A);
+			count++;
+		}
+		if (sb.toString().contains(B))
+			return count;
+		if (sb.append(A).toString().contains(B))
+			return ++count;
+		return -1;
+	}
+}
