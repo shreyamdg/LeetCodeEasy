e0d51e15c50fc352a95a9f3ff4f62358c1f805ed
shreyam15 <shreyam.duttagupta@gmail.com>
2018-09-19 22:45:58 -0500

    code to check if the robot returns back to 0,0

--- src/arraysAndstrings/RobotReturnToOrigin.java
+++ src/arraysAndstrings/RobotReturnToOrigin.java
@@ -1,5 +1,28 @@
 package arraysAndstrings;
 
 public class RobotReturnToOrigin {
+	public boolean judgeCircle(String moves) {
+		int i = 0;
+		int j = 0;
 
+		for (char ch : moves.toCharArray()) {
+			if (ch == 'U') {
+				i++;
+			} else if (ch == 'D') {
+				i--;
+			} else if (ch == 'L') {
+				j--;
+			} else if (ch == 'R') {
+				j++;
+			} else {
+				return false;
+			}
+		}
+
+		if (i == 0 && j == 0) {
+			return true;
+		}
+
+		return false;
+	}
 }
