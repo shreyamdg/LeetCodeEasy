e401223a502bb85df663442b0808176464118cc2
shreyam15 <shreyam.duttagupta@gmail.com>
2018-09-23 20:44:59 -0500

    another solution

--- src/bitmanipulation/Hammingweight.java
+++ src/bitmanipulation/Hammingweight.java
@@ -9,4 +9,13 @@ public class Hammingweight {
 		}
 		return count;
 	}
+
+	public int hammingWeight2(int n) {
+		int count = 0;
+		while (n != 0) {
+			n = n & (n - 1);
+			count += 1;
+		}
+		return count;
+	}
 }
